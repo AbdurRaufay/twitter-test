dbd761dfca9d3166cad963e43f3d599fbb735569
abdurrauf <abdurrauf0306@gmail.com>
2023-08-02 17:23:39 -0700

    f

--- pages/twitter/index.js
+++ pages/twitter/index.js
@@ -1,76 +1,67 @@
-
-// // pages/index.js
 // import React from 'react';
-// import Head from 'next/head';
-// // import  BsTwitter  from 'react-icons/bs';
-// import axios from 'axios';
-
 // const Twitter = () => {
-//   const handleTwitterLogin = async () => {
-//     try {
-//       const response = await axios.post('http://localhost:8080/auth/twitter/register');
-//       const { user } = response.data;
-//       console.log('Registered User:', user);
+//     const handleTwitterLogin = async () => {
+//         try {
+//           window.location.href = 'http://localhost:8080/auth/twitter';
+//         } catch (error) {
+//           console.error('Twitter Login Error:', error);
+//         }
+//       };
+    
+//       return (
+//         <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+//           <div className="bg-white p-8 rounded shadow-md">
+//             <h1 className="text-2xl font-bold mb-4">Welcome to Twitter Login</h1>
+//             <button
+//               onClick={handleTwitterLogin}
+//               className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded flex items-center"
+//             >
+//              Login with Twitter
+//             </button>
+//           </div>
+//         </div>
+//       );
+//     };
 
-//       // After successful registration, proceed with the login
-//       const loginResponse = await axios.get('http://localhost:8080/auth/twitter/login');
-//       const { authenticatedUser } = loginResponse.data;
-//       console.log('Authenticated User:', authenticatedUser);
+// export default Twitter;
 
-//       // Handle the authenticated user data or redirect to the dashboard page as needed
-//     } catch (error) {
-//       console.error('Twitter Login/Register Error:', error);
-//     }
-//   };
-
-//   return (
-//     <div className="bg-gray-100 min-h-screen flex items-center justify-center">
-//       <Head>
-//         <title>Twitter Login</title>
-//       </Head>
-//       <div className="bg-white p-8 rounded shadow-md">
-//         <h1 className="text-2xl font-bold mb-4">Welcome to Twitter Login</h1>
-//         <button
-//           onClick={handleTwitterLogin}
-//           className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded flex items-center"
-//         >
-//             login with twitter
-//           {/* <BsTwitter className="mr-2" /> Login with Twitter */}
-//         </button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Twitter
 
 import React from 'react';
-import Head from 'next/head';
-import axios from 'axios';
 
 const Twitter = () => {
-    const handleTwitterLogin = async () => {
-        try {
-          // Make a request to the backend's /auth/twitter route
-          window.location.href = 'http://localhost:8080/auth/twitter';
-        } catch (error) {
-          console.error('Twitter Login Error:', error);
-        }
-      };
-    
-      return (
-        <div className="bg-gray-100 min-h-screen flex items-center justify-center">
-          <div className="bg-white p-8 rounded shadow-md">
-            <h1 className="text-2xl font-bold mb-4">Welcome to Twitter Login</h1>
-            <button
-              onClick={handleTwitterLogin}
-              className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded flex items-center"
-            >
-             Login with Twitter
-            </button>
-          </div>
-        </div>
-      );
-    };
+  const handleTwitterLogin = async () => {
+    try {
+      // Open a pop-up window for Twitter login
+      const popup = window.open('http://localhost:8080/auth/twitter', '_blank', 'width=600,height=400');
+      if (popup) {
+        // Listen for messages from the pop-up window
+        window.addEventListener('message', (event) => {
+          if (event.origin === 'http://localhost:8080' && event.data === 'twitter-auth-success') {
+            // Handle successful authentication, e.g., redirect to a logged-in page
+            window.location.href = 'https://marvelous-churros-c678e1.netlify.app';
+          }
+        });
+      } else {
+        console.error('Pop-up blocked. Please allow pop-ups for this website.');
+      }
+    } catch (error) {
+      console.error('Twitter Login Error:', error);
+    }
+  };
+
+  return (
+    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+      <div className="bg-white p-8 rounded shadow-md">
+        <h1 className="text-2xl font-bold mb-4">Welcome to Twitter Login</h1>
+        <button
+          onClick={handleTwitterLogin}
+          className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded flex items-center"
+        >
+          Login with Twitter
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Twitter;
