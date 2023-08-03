aa23788a747b89c869421752d40c250ef5db9924
abdurrauf <abdurrauf0306@gmail.com>
2023-08-03 16:52:42 -0700

    last commit

---
+++ pages/privacypolicy/Privacy.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import CookieConsent, { Cookies } from "react-cookie-consent";
+const Privacy = () => {
+    return (
+        <div>
+            <p>
+                This Privacy Policy explains how we collect, use, and protect the personal information you provide to us when you use our Testing App web application.
+
+                Information We Collect
+
+                When you use our App, we may collect the following types of personal information:
+
+                Name: We may collect your name, which you provide to us during the account registration process or through account settings.
+
+                Email: We may collect your email address, which you provide to us during the account registration process or when contacting us for support.
+                 
+                How We Use Your Information
+
+                We use the collected information for the following purposes:
+
+                Personalization: We may use your name to personalize your experience within the App.
+
+                Communication: We may use your email address to communicate with you, respond to your inquiries, and provide you with important updates and information related to the App.
+
+                Customer Support: Your name and email address may be used to assist you with any support requests or issues you may have while using the App.
+
+                Analytics: We may use anonymous and aggregated data for analytical purposes to understand how users interact with the App, improve our services, and enhance the user experience.
+
+                Data Security
+
+                We take data security seriously and employ reasonable security measures to protect your personal information from unauthorized access, alteration, disclosure, or destruction.
+
+                Data Sharing
+
+                We do not sell, trade, or rent your personal information to third parties. However, we may share your personal information with trusted service providers who assist us in operating the App and delivering services to you. These service providers are obligated to keep your information confidential and may not use it for any purpose other than providing services to us.
+
+                Your Choices and Rights
+
+                You have the right to access, update, or delete your personal information. If you wish to do so or have any concerns about the data we hold about you, please contact us at abdurrauf0306@gmail.com.
+
+                Children's Privacy
+
+                Our App is not intended for use by children under the age of 18. We do not knowingly collect personal information from children under 18. If you are a parent or guardian and believe that your child has provided us with personal information, please contact us, and we will promptly delete such information from our records.
+
+                Changes to this Privacy Policy
+
+                We may update this Privacy Policy from time to time. We will notify you of any significant changes by posting the updated Privacy Policy on this page or by other means as required by applicable law. Please review this Privacy Policy periodically to stay informed about how we collect, use, and protect your information.
+
+                Contact Us
+
+                If you have any questions or concerns about this Privacy Policy or our data practices, please contact us at abdurrauf0306@gmail.com.
+            </p>
+        </div>
+    )
+}
+
+export default Privacy
---
+++ pages/termsofservices/TermsOfServices.js
@@ -0,0 +1,60 @@
+import React from 'react'
+
+const TermsOfServices = () => {
+  return (
+    <div>
+        <P>
+        Terms of Service
+
+Last Updated: 8/3/2023
+
+Welcome to Testing App Web application  By using our App, you agree to these Terms of Service. Please read them carefully.
+
+1. Use of the App
+
+You may use the App only for lawful purposes and in accordance with these Terms of Service. By accessing or using the App, you represent and warrant that you are at least 18 years old and have the legal capacity to enter into these Terms of Service.
+
+2. User Accounts
+
+You may need to create a user account to access certain features of the App. You are responsible for maintaining the confidentiality of your account credentials and agree to accept responsibility for all activities that occur under your account.
+
+3. Intellectual Property
+
+The App and its original content, features, and functionality are owned by Alimcosoft and are protected by international copyright, trademark, patent, trade secret, and other intellectual property or proprietary rights laws.
+
+4. User Content
+
+By using the App, you may have the opportunity to submit, post, or display content ("User Content"). You grant  a non-exclusive, worldwide, royalty-free, perpetual, irrevocable, and fully sublicensable right to use, reproduce, modify, adapt, publish, translate, create derivative works from, distribute, and display such User Content in any media, format, or platform.
+
+5. Prohibited Conduct
+
+You agree not to engage in any of the following prohibited activities:
+
+Violating any applicable laws or regulations.
+Impersonating any person or entity or misrepresenting your affiliation with any person or entity.
+Interfering with the proper functioning of the App or attempting to gain unauthorized access to any system, data, or network.
+6. Privacy
+
+Your privacy is important to us. Please review our Privacy Policy to understand how we collect, use, and protect your personal information.
+
+9. Changes to the Terms
+
+We may update these Terms of Service from time to time. By continuing to use the App after any changes to these Terms, you agree to be bound by the revised Terms.
+
+10. Termination
+
+We may terminate or suspend your access to the App immediately, without prior notice or liability, for any reason whatsoever.
+
+11. Governing Law
+
+These Terms of Service shall be governed by and construed in accordance with the laws of [Your Country/State].
+
+Contact Us
+
+If you have any questions or concerns about these Terms of Service, please contact us at abdurrauf0306gmail.com.
+        </P>
+    </div>
+  )
+}
+
+export default TermsOfServices
