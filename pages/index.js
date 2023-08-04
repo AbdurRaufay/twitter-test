0ab076f4c7cbb003d6a5d8f232d6bc42883713c3
abdurrauf <abdurrauf0306@gmail.com>
2023-08-03 17:47:03 -0700

    last commit

--- pages/index.js
+++ pages/index.js
@@ -2,22 +2,16 @@
 import { Inter } from 'next/font/google'
 import Twitter from './twitter'
 import Link from 'next/link'
-import { useRouter } from 'next/router'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  const router=useRouter()
-  const handlePrivacy = () => {
-    router.push('/privacypolicy/[id]',);
-  };
- const handleTermsOfServices=()=>{
-  router.push("/termsofservices/[id]")
- }
+
   return (
     <div>
-      {/* <Link href="pages/privacypolicy/Privacy">Privacy Policy</Link> */}
-      <button className='p-8 text-12 ' onClick={handlePrivacy}>Privacy</button>
-      <button className='p-8 text-12 ' onClick={handleTermsOfServices}>TermsOfServices</button>
+      <div className='flex justify-around'>
+      <Link href="/privacypolicy">Privacy Policy</Link>
+      <Link href="/termsofservices">TermsOfServices</Link>
+      </div>
       <div>
       <Twitter/>
       </div>
